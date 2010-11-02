9527bbdb3e933fbabb03eef2bd749179e1735b01
burbon <burbon@cristin.lan>
2010-11-03 00:27:28 +0100

    bashrc and xmonad changes

---
+++ .bashrc
@@ -0,0 +1,26 @@
+# Check for an interactive session
+[ -z "$PS1" ] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+
+
+########### PROMPT ############
+PS1='[\u@\h \W]\$ '
+
+PROMPT_COMMAND='DIR=`pwd|sed -e "s!$HOME!~!"`; if [ ${#DIR} -gt 30 ]; then CurDir=${DIR:0:12}...${DIR:${#DIR}-15}; else CurDir=$DIR; fi'
+
+F_TIME="\[\033[00;36m\]\t\[\033[00;00m\]"
+F_NAME="\[\033[00;32m\]\u\[\033[00m\]@\[\033[00;33m\]\h\[\033[00;00m\]"
+F_DIR="\[\033[00;36m\]\$CurDir\[\033[00;00m\]"
+F_GIT='$(__git_ps1 " (%s)")'
+
+PS1="┌─[$F_TIME]─[$F_NAME]─[$F_DIR]$F_GIT\n└─]\$ "
+
+########### END_PROMPT ############
+
+export HISTCONTROL=ignoredups
+export EDITOR=vim
+export MPD_HOST="wpiszcos@localhost"
+
+export PATH=$HOME/bin:$PATH
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,38 +1,62 @@
 import XMonad
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Accordion
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Grid
+import XMonad.Layout.Cross
+
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-import XMonad.Layout.Grid
+
+import XMonad.Hooks.SetWMName
  
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
-    , className =? "Pidgin"    --> doFloat
+    , className =? "Pidgin" --> doFloat
+
+    , className =? "Namoroka" --> doShift "2"
+    , className =? "Opera" --> doShift "2"
+
+    , className =? "Pidgin" --> doShift "3"
+
+    , className =? "Shredder" --> doShift "4"
+    , className =? "Lanikai" --> doShift "4"
+
+    , className =? "Sonata" --> doShift "5"
+
+    , className =? "Krusader" --> doShift "6"
+
+    , className =? "MPlayer" --> doShift "7"
     ]
  
-main = do
-    xmproc <- spawnPipe "/home/bc5042/bin/xmobar /home/bc5042/.xmobarrc"
-    xmonad $ defaultConfig {
-        terminal = "urxvt", 
-        modMask = mod4Mask,     -- Rebind Mod to the Windows key
-        borderWidth = 1,
-        focusFollowsMouse = False,
-        --workspaces = ["code", "web", "im"] ++ map show [4..9],
+--myLayoutHook = Full ||| Accordion
 
-        manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
-                        <+> manageHook defaultConfig,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig,
-        logHook = dynamicLogWithPP $ xmobarPP
+myLayoutHook = avoidStruts ( Full ||| smartBorders Full ||| Tall 1 0.03 0.5 ||| Accordion ||| simpleTabbed ||| Grid ||| simpleCross )
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/burbon/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                        <+> manageHook defaultConfig
+        --, layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayoutHook
+        , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        } `additionalKeys` [
-            ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock"),
-            ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
-            ((0, xK_Print), spawn "scrot")
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , startupHook = setWMName "LG3D"
+        , terminal = "urxvt"
+        , focusFollowsMouse = False
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
         ]
-
-
 
