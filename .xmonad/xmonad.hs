89047f5654b2e4d8cb5e90f7f83c205c4b33e08e
burbon <blazej.cegielka@gmail.com>
2013-01-11 17:20:34 +0000

    xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Accordion
@@ -11,28 +12,29 @@ import XMonad.Layout.Cross
 
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+
 import System.IO
 
-import XMonad.Hooks.SetWMName
  
 myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    , className =? "Pidgin" --> doFloat
+    [
+        className =? "Gimp"      --> doFloat,
+        className =? "Vncviewer" --> doFloat,
+        className =? "Pidgin" --> doFloat,
+        className =? "Skype" --> doFloat,
 
-    , className =? "Namoroka" --> doShift "2"
-    , className =? "Opera" --> doShift "2"
+        className =? "Firefox" --> doShift "2",
+        className =? "Opera" --> doShift "2",
+        className =? "Thunderbird" --> doShift "2",
 
-    , className =? "Pidgin" --> doShift "3"
+        className =? "Pidgin" --> doShift "3",
+        className =? "Skype" --> doShift "4",
 
-    , className =? "Shredder" --> doShift "4"
-    , className =? "Lanikai" --> doShift "4"
+        className =? "Sonata" --> doShift "5",
 
-    , className =? "Sonata" --> doShift "5"
+        className =? "Krusader" --> doShift "6",
 
-    , className =? "Krusader" --> doShift "6"
-
-    , className =? "MPlayer" --> doShift "7"
+        className =? "MPlayer" --> doShift "7"
     ]
  
 --myLayoutHook = Full ||| Accordion
@@ -40,23 +42,23 @@ myManageHook = composeAll
 myLayoutHook = avoidStruts ( Full ||| smartBorders Full ||| Tall 1 0.03 0.5 ||| Accordion ||| simpleTabbed ||| Grid ||| simpleCross )
 
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/burbon/.xmobarrc"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
-                        <+> manageHook defaultConfig
-        --, layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , layoutHook = myLayoutHook
-        , logHook = dynamicLogWithPP $ xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , startupHook = setWMName "LG3D"
-        , terminal = "urxvt"
-        , focusFollowsMouse = False
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/burb/.xmobarrc"
+    xmonad $ defaultConfig {
+        manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+            <+> manageHook defaultConfig,
+        --layoutHook = avoidStruts  $  layoutHook defaultConfig
+        layoutHook = myLayoutHook,
+        logHook = dynamicLogWithPP $ xmobarPP {
+            ppOutput = hPutStrLn xmproc,
+            ppTitle = xmobarColor "green" "" . shorten 50
+        },
+        --modMask = mod4Mask     -- Rebind Mod to the Windows key
+        startupHook = setWMName "LG3D",
+        terminal = "urxvt",
+        focusFollowsMouse = False
+    } `additionalKeys`
+        [
+            ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
+            ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
+            ((0, xK_Print), spawn "scrot")
         ]
-
